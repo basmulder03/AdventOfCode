49d39b92212696305b77570d2a241d0f24810763
basmulder03 <bas.d.mulder@gmail.com>
2023-03-21 10:33:52 +0100

    finish day 6

--- 2015/day6.py
+++ 2015/day6.py
@@ -1,5 +1,56 @@
 def solve_part_1(input_data):
-    pass
+    matrix = [[0 for _ in range(1000)] for _ in range(1000)]
+    for line in input_data.strip().split("\n"):
+        actions = line.split(" ")
+        if actions[0] == 'turn':
+            state, from_coor, _, to_coor = actions[1:]
+        else:
+            state, from_coor, _, to_coor = actions
+        from_x, from_y = [int(n) for n in from_coor.split(",")]
+        to_x, to_y = [int(n) for n in to_coor.split(",")]
+        if state == 'toggle':
+            for x in range(from_x, to_x + 1):
+                for y in range(from_y, to_y + 1):
+                    matrix[y][x] = not matrix[y][x]
+        elif state == 'off':
+            for x in range(from_x, to_x + 1):
+                for y in range(from_y, to_y + 1):
+                    matrix[y][x] = 0
+        elif state == 'on':
+            for x in range(from_x, to_x + 1):
+                for y in range(from_y, to_y + 1):
+                    matrix[y][x] = 1
+    total_count = 0
+    for x_list in matrix:
+        total_count += x_list.count(1)
+    return total_count
+
 
 def solve_part_2(input_data):
-    pass
+    matrix = [[0 for _ in range(1000)] for _ in range(1000)]
+    for line in input_data.strip().split("\n"):
+        actions = line.split(" ")
+        if actions[0] == 'turn':
+            state, from_coor, _, to_coor = actions[1:]
+        else:
+            state, from_coor, _, to_coor = actions
+        from_x, from_y = [int(n) for n in from_coor.split(",")]
+        to_x, to_y = [int(n) for n in to_coor.split(",")]
+        if state == 'toggle':
+            for x in range(from_x, to_x + 1):
+                for y in range(from_y, to_y + 1):
+                    matrix[y][x] += 2
+        elif state == 'off':
+            for x in range(from_x, to_x + 1):
+                for y in range(from_y, to_y + 1):
+                    matrix[y][x] -= 1
+                    if matrix[y][x] < 0:
+                        matrix[y][x] = 0
+        elif state == 'on':
+            for x in range(from_x, to_x + 1):
+                for y in range(from_y, to_y + 1):
+                    matrix[y][x] += 1
+    total_count = 0
+    for x_list in matrix:
+        total_count += sum(x_list)
+    return total_count
---
+++ 2015/day7.py
@@ -0,0 +1,5 @@
+def solve_part_1(input_data):
+    pass
+
+def solve_part_2(input_data):
+    pass
